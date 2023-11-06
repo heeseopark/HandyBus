4c335f994eb266a28843b19870410fc97000ca17
heeseopark <heeseopark@korea.ac.kr>
2023-11-06 13:52:07 +0900

    Feat: initialize project

---
+++ src/main/java/HandyBus/HandyBus/HandyBusApplication.java
@@ -0,0 +1,13 @@
+package HandyBus.HandyBus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HandyBus/HandyBus/HandyBusApplicationTests.java
@@ -0,0 +1,13 @@
+package HandyBus.HandyBus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
